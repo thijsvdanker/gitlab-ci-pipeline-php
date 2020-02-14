ed8b0686ece801ecaf804b2299984266fe5d9068
Thijs van den Anker <thijs@bitsnbolts.nl>
2020-02-14 10:56:47 +0100

    Fix scripts

--- php/scripts/packages.sh
+++ php/scripts/packages.sh
@@ -41,5 +41,5 @@ DEBIAN_FRONTEND=noninteractive
       unzip \
       zip \
       zlib1g-dev \
-      libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+      libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb \
       && rm -rf /var/lib/apt/lists/*
