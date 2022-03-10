df40b2b0c1be36fc0482040cd7e58664120fb3af
Thijs van den Anker <thijs@bitsnbolts.nl>
2022-03-10 15:25:49 +0100

    Install odbc drivers for mssql connections

--- php/scripts/packages.sh
+++ php/scripts/packages.sh
@@ -43,6 +43,7 @@ export DEBIAN_FRONTEND=noninteractive
       && curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
       && curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list \
       && apt-get update \
+      && ACCEPT_EULA=Y apt-get install -y msodbcsql18
       && apt-get install -yq \
       unixodbc-dev \
       && rm -rf /var/lib/apt/lists/*
