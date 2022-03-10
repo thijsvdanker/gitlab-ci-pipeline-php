7b70362cf395b7eee6a560e026030f3001e099c4
Thijs van den Anker <thijs@bitsnbolts.nl>
2022-03-10 21:14:21 +0100

    Include mssql tools

--- php/scripts/packages.sh
+++ php/scripts/packages.sh
@@ -44,6 +44,9 @@ export DEBIAN_FRONTEND=noninteractive
       && curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list \
       && apt-get update \
       && ACCEPT_EULA=Y apt-get install -y msodbcsql18 \
+      && ACCEPT_EULA=Y apt-get install -y mssql-tools18 \
+      && echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc \
+      && source ~/.bashrc
       && apt-get install -yq \
       unixodbc-dev \
       && rm -rf /var/lib/apt/lists/*
